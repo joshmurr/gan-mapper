1086dd5561d75cb0e83441bfebfdeeb4f6d5f414
Josh Murr <joshuadavidmurr@gmail.com>
2022-01-14 11:24:01 +0000

    Fix lpips import probs

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+import os
+from .stylegan2 import projector
+
 from flask import Blueprint, render_template
-routes_blueprint = Blueprint('routes_blueprint', __name__)
+
+routes_blueprint = Blueprint("routes_blueprint", __name__)
 
 
-@routes_blueprint.route('/')
+@routes_blueprint.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@routes_blueprint.route("/run_projector")
+def run_projector():
+    sample_image = "/sample_images/me.jpg"
+    img_path = os.path.abspath(os.path.dirname(__file__) + sample_image)
+    model_ckpt = "/model_checkpoints/stylegan-128px.model"
+    ckpt_path = os.path.abspath(os.path.dirname(__file__) + model_ckpt)
+    projector.project([img_path], ckpt_path)
+
+    return render_template("projector.html")
